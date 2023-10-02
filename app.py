9bab9ecfc331b130bba7b6d00a0ac68eb47e8397
Roman Wang <romanwang888@gmail.com>
2023-10-02 01:24:19 -0700

    add logging to python

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
+import logging
 from flask import Flask, request
 
 app = Flask(__name__)
 
+logging.basicConfig(level=logging.DEBUG,  # Set the logging level
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger('my_logger')
+logger.setLevel(logging.DEBUG)
 
 @app.route('/argoDeployment', methods=['POST'])
 def post_data():
     if request.method == 'POST':
         data = request.json
         print("Received data: ", data)
+        logger.info("Received data: %s", data)
         return "Data received successfully", 200
     else:
         return "Invalid request method", 405
